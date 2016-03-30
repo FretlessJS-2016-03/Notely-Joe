d2d14e8c829b9a88766507c9184420cdd0757146
Joe Calvin <joecalvin7@gmail.com>
2016-03-30 14:09:25 -0400

    Refactor Code

--- Server/app.js
+++ Server/app.js
@@ -1,42 +1,19 @@
 var express = require('express');
 var notelyServerApp = express();
-
-var db = require('mongoose');
-db.connect('mongodb://localhost:27017/notely');
-
-var NoteSchema = db.Schema({
-  title: String,
-  body_html: String
-});
-
-var Note = db.model('Note', NoteSchema);
+var Note = require('./models/note');
 
 // Cross-Origin Resource Sharing (CORS) middleware
-notelyServerApp.use(function(req, res, next)
-  {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
+notelyServerApp.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+notelyServerApp.get('/', function(req, res) {
+  Note.find().then(function(notes) {
+    res.json(notes);
   });
+});
 
-  notelyServerApp.get('/', function(req, res) {
-    Note.find().then(function(notes) {
-      res.json(notes);
-    });
-
-    // res.json([
-    //   {
-    //     title: 'Edited hardcoded note!',
-    //     body_html: 'Cool note. Aww, shucks.'
-    //   },
-    //   {
-    //     title: 'Another edited hardcoded note',
-    //     body_html: "Ain't life grand?"
-    //   }
-    // ]);
-
-  });
-
-  notelyServerApp.listen(3030, function()
-  {
-    console.log('Listening on http://Localhost:3030');
-  });
+notelyServerApp.listen(3030, function() {
+  console.log('Listening on http://localhost:3030');
+});

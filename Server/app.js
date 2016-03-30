b2713903745b7dd565c7f81899289c33946a3ff7
Joe Calvin <joecalvin7@gmail.com>
2016-03-30 13:40:19 -0400

    Add mongo

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var notelyServerApp = express();
 
+var db = require('mongoose');
+db.connect('mongodb://localhost:27017/notely');
+
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String
+});
+
+var Note = db.model('Note', NoteSchema);
+
 // Cross-Origin Resource Sharing (CORS) middleware
 notelyServerApp.use(function(req, res, next)
   {
@@ -8,22 +18,21 @@ notelyServerApp.use(function(req, res, next)
     next();
   });
 
-notelyServerApp.get('/', function(req, res)
-  {
-    res.json([
-      {
-        title: 'Edited Harcdoded Note',
-        body_html: 'Cool note.  Aww shucks!'
-      },
-      {
-        title: 'Another edited hardcoded note',
-        body_html: "Ain't life grand!"
-      },
-      {
-        title: 'Yet Another hardcoded note',
-        body_html: "THis is getting monotonous!"
-      }
-    ]);
+  notelyServerApp.get('/', function(req, res) {
+    Note.find().then(function(notes) {
+      res.json(notes);
+    });
+
+    // res.json([
+    //   {
+    //     title: 'Edited hardcoded note!',
+    //     body_html: 'Cool note. Aww, shucks.'
+    //   },
+    //   {
+    //     title: 'Another edited hardcoded note',
+    //     body_html: "Ain't life grand?"
+    //   }
+    // ]);
 
   });
 
